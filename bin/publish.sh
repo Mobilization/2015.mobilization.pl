c3573951dca73a9ca2c5dc4cba1d38e9f2dfeb88
Marcin Iwanicki <marcin.iwanicki@appliwings.com>
2015-06-07 16:45:17 +0200

    Generated page based on revision a43756809e83e22643b1de42f909828b67aadc19

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-GIT_STATUS_LINES=$(git status --porcelain | wc -l)
-if [ $GIT_STATUS_LINES -ge 1 ] ; then
- 	echo "Error: Cannot publish changes. Some of the files are not committed. See git status to get more details.";
- 	git status;
-	exit 1;
-fi
+# GIT_STATUS_LINES=$(git status --porcelain | wc -l)
+# if [ $GIT_STATUS_LINES -ge 1 ] ; then
+# 	echo "Error: Cannot publish changes. Some of the files are not committed. See git status to get more details.";
+# 	git status;
+#	exit 1;
+# fi
 
 GIT_CURRENT_REVISION=$(git rev-parse --verify HEAD)
 GIT_COMMIT_MESSAGE="Generated page based on revision "$GIT_CURRENT_REVISION
