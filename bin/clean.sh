f54be4b45f34e66c37205455203bc40559ca4440
Marcin Iwanicki <marcin.iwanicki@appliwings.com>
2015-06-07 10:24:40 +0200

    Created script to clean up the out (gh-pages branch) folder.

---
+++ bin/clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# clean 2015.mobilization.pl-gh-pages directory
+docpad clean
+
+# create folder for gh-pages branch
+mkdir ../2015.mobilization.pl-gh-pages
+cd ../2015.mobilization.pl-gh-pages
+git clone https://github.com/mobilization5/2015.mobilization.pl.git .
+git checkout gh-pages
