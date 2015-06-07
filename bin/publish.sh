a43756809e83e22643b1de42f909828b67aadc19
Marcin Iwanicki <marcin.iwanicki@appliwings.com>
2015-06-07 16:43:14 +0200

    Implemented pushing page to gh-pages branch.

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,5 +2,14 @@
 
 GIT_STATUS_LINES=$(git status --porcelain | wc -l)
 if [ $GIT_STATUS_LINES -ge 1 ] ; then
-	echo "FUCK";
+ 	echo "Error: Cannot publish changes. Some of the files are not committed. See git status to get more details.";
+ 	git status;
+	exit 1;
 fi
+
+GIT_CURRENT_REVISION=$(git rev-parse --verify HEAD)
+GIT_COMMIT_MESSAGE="Generated page based on revision "$GIT_CURRENT_REVISION
+
+cd ../mobilization.pl-gh-pages
+git add -A && git commit -m "$GIT_COMMIT_MESSAGE"
+git push origin gh-pages
